90099bad099a88eee7f795803c086651d88c08f2
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-02-10 17:44:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # config.py
 
 # Bot token obtained from BotFather
-BOT_TOKEN = 'your_bot_token'
+BOT_TOKEN = '6502103329:AAFLhdsQ2OdtmNKOh1lITyQNPFX4XKHgJ44'
 
 # Owner's Telegram user ID
-OWNER_ID = 123456789  # Replace with your owner's Telegram user ID
+OWNER_ID =5911205161 # Replace with your owner's Telegram user ID
 
 # List of sudo users' Telegram user IDs
-SUDO_USERS = [OWNER_ID]
+SUDO_USERS = [5911205161]
 
 # Heroku app name (optional)
 HEROKU_APP_NAME = 'your-heroku-app-name'  # Replace with your Heroku app name
 
 # Heroku API key (optional, required only if deploying to Heroku)
-HEROKU_API_KEY = 'your-heroku-api-key'  # Replace with your Heroku API key
+HEROKU_API_KEY = '23874dd3-b04c-4a69-9b21-e40e5d675d20'  # Replace with your Heroku API key
