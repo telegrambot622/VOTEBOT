d156db6ab67a7d70eca56af6a67e78a954eaf058
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-02-10 18:23:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,23 @@
-# config.py
+import os
+from telethon import TelegramClient, events, Button
+import json
+
+# Read configurations from app.json file
+with open('app.json') as f:
+    app_config = json.load(f)
 
 # Bot token obtained from BotFather
-BOT_TOKEN = '6502103329:AAFLhdsQ2OdtmNKOh1lITyQNPFX4XKHgJ44'
+BOT_TOKEN = app_config.get('BOT_TOKEN')
 
 # Owner's Telegram user ID
-OWNER_ID =5911205161 # Replace with your owner's Telegram user ID
+OWNER_ID = app_config.get('OWNER_ID')
 
 # List of sudo users' Telegram user IDs
-SUDO_USERS = [5911205161]
+SUDO_USERS = app_config.get('SUDO_USERS')
 
 # Heroku app name (optional)
-HEROKU_APP_NAME = 'your-heroku-app-name'  # Replace with your Heroku app name
+HEROKU_APP_NAME = app_config.get('HEROKU_APP_NAME')
 
 # Heroku API key (optional, required only if deploying to Heroku)
-HEROKU_API_KEY = '23874dd3-b04c-4a69-9b21-e40e5d675d20'  # Replace with your Heroku API key
+HEROKU_API_KEY = app_config.get('HEROKU_API_KEY')
+
